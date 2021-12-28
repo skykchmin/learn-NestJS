69817570ac8a868ac6b456722b39aff24eb0db78
skykchmin <skykchmin@naver.com>
2021-12-28 23:50:34 +0900

    [Add] Learn how to run typescript - 211228
    타입스크립트 동작 방법 익히기

---
+++ TypeChain/index.ts
@@ -0,0 +1,12 @@
+const name = 'eric',
+    age = 24,
+    gender = "male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+// sayHi(name, age, gender);
+sayHi(name, age);
+
+export {};
