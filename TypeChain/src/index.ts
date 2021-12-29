aa684df8dcf5824a1424a0425aa73dfb0897253c
skykchmin <skykchmin@naver.com>
2021-12-29 21:32:07 +0900

    [add] validation for Block - 211229
    블록체인 validation 처리
    접근제어자, 클래스, static

--- TypeChain/src/index.ts
+++ TypeChain/src/index.ts
@@ -1,32 +1,95 @@
-interface Human {
-    name: string; 
-    age: number;
-    gender: string;
+import * as CryptoJS from "crypto-js";
+
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    static calculateBlockHash = (
+        index: number, 
+        previousHash: string,
+        timestamp: number,
+        data: string
+          ): string => 
+        CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    
+    static validateStructure = (aBlock: Block) : boolean => 
+        typeof aBlock.index === "number" && 
+        typeof aBlock.hash === "string" && 
+        typeof aBlock.previousHash === "string" &&
+        typeof aBlock.timestamp === "number" &&
+        typeof aBlock.data === "string";
+    
+
+    constructor(index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number){
+            this.index = index;
+            this.hash = hash;
+            this.previousHash = previousHash;
+            this.data = data;
+            this.timestamp = timestamp;
+        }
 }
 
-const person = {
-    name: "eric",
-    age: 25,
-    gender: "male"
+const genesisBlock: Block = new Block(0, "202020202020", "", "Hello", 123456);
+
+let blockchain:[Block] = [genesisBlock]; // Block이 연결되어있는 array 구조
+
+// blockchain.push("stuff")
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data:string): Block => {
+    const previousBlock: Block = getLatestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimeStamp: number = getNewTimeStamp();
+    const nextHash: string = Block.calculateBlockHash(
+        newIndex,
+        previousBlock.hash,
+        newTimeStamp,
+        data
+    );
+
+    const newBlock : Block = new Block(newIndex, nextHash, previousBlock.hash, data, newTimeStamp);
+    addBlock(newBlock);
+    return newBlock;
+};
+
+const getHashforBlock = (aBlock: Block) : string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
+const isBlockValid = (candidateBlock : Block, previousBlock: Block) : boolean => {
+    if(!Block.validateStructure(candidateBlock)){
+        return false
+    } else if (previousBlock.index + 1 !== candidateBlock.index){
+        return false;
+    } else if (previousBlock.hash !== candidateBlock.previousHash){
+        return false;
+    } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash){
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const addBlock = (candidateBlock: Block) : void => {
+    if(isBlockValid(candidateBlock, getLatestBlock())){
+        blockchain.push(candidateBlock);
+    }
 }
 
-const name = 'eric',
-    age = 24,
-    gender = "male";
+createNewBlock("second");
+createNewBlock("third");
 
-// 인자를 파싱해서 전달해주는 방법 
-// const sayHi = (name: string, age: number , gender?: string): string => {    
-//     return `Hello ${name}, you are ${age}, you are a ${gender}`;
-// };
-
-// console.log(sayHi(name, age, gender));
-
-// 인터페이스로 전달하는 방법
-const sayHi = (Person: Human): string => {
-    return `Hello ${Person.name}, you are ${Person.age}, you are a ${Person.gender}`;   // interface의 속성을 파싱해서 전달해준다
-}
-
-console.log(sayHi(person));
-// sayHi(name, "444", age);
+console.log(blockchain);
+// console.log(blockchain);
 
 export {};
